50ae81a907df08a35bca07b9b4c74cc6043e4b64
RSpeckles <rchenspeckman@gmail.com>
2022-10-04 12:20:36 -0700

    Completed lab

--- main.py
+++ main.py
@@ -2,3 +2,18 @@
 # ******************************
 # Make your Code
 # ******************************
+
+numbers = []
+
+while len(numbers) < 5:
+    try:
+        numbers.append(int(input("Enter a number: ")))
+    except ValueError:
+        print("Value must be numeric")
+
+average = sum(numbers) / len(numbers)
+
+for i in range(5):
+    if numbers[i] > average:
+        print(numbers[i], end=' ')
+
